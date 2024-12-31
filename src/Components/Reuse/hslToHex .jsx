79c315ed9adaa8322d710624b7645c7621838c1b
Abdur-Rahman-s <arsw5649@gmail.com>
2024-12-31 17:53:49 +0600

    The Reuse folder contains files for converting hsl to rgb and hsl to hex, which are used in all color type generators.

---
+++ src/Components/Reuse/hslToHex .jsx
@@ -0,0 +1,15 @@
+export const hslToHex = (h, s, l) => {
+    s /= 100;
+    l /= 100;
+    const k = (n) => (n + h / 30) % 12;
+    const a = s * Math.min(l, 1 - l);
+    const f = (n) =>
+        l - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
+    const r = Math.round(255 * f(0));
+    const g = Math.round(255 * f(8));
+    const b = Math.round(255 * f(4));
+    return `#${((1 << 24) + (r << 16) + (g << 8) + b)
+        .toString(16)
+        .slice(1)
+        .toUpperCase()}`;
+};
